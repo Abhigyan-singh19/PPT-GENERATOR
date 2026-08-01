c1ca433b29cce4ab9bf6828262790678833e5331
Abhigyan-singh19 <singhabhigyan922@gmail.com>
2026-08-01 11:48:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+# step 1 load modules 
 import os
 import time
 import langchain
@@ -10,22 +10,20 @@ from tavily import TavilyClient
 from langchain.messages import SystemMessage, HumanMessage
 import numpy as np
 import streamlit as st
-from langchain_community.document_loaders import PyMuPDFLoader
 
-#======== STEP 2: Streamlit Front-end==============
-# To show web-app : complete page layout
-st.set_page_config(layout="wide")
-
+#=========================== STEP 2 =============================
+st.set_page_config(layout = "wide")
 st.title("AI PPT GENERATOR")
 st.divider()
 st.sidebar.title("ENTER API-KEYS")
 
+#========step 3 ====================
 
-# ======== Step 3 Load Api key===============
-GOOGLE_API_KEY = st.sidebar.text_input("Google_API_KEY", type ="password")
-TAVILY_API_KEY = st.sidebar.text_input("TAVILY_API_KEY", type ="password")
+GOOGLE_API_KEY = st.sidebar.text_input("GOOGLE_API_KEY", type = "password")
+TAVILY_API_KEY = st.sidebar.text_input("TAVILY_API_KEY", type = "password")
 
-# ============API VALIDATIONS=============
+
+#============API VALIDATIONS ============
 ALL_API = [GOOGLE_API_KEY,TAVILY_API_KEY]
 
 if not all(ALL_API):
@@ -43,68 +41,68 @@ elif all(ALL_API):
   )
 else:
   st.sidebar.info("check api keys")
-  #=========== back end code===============
 
-  # Search_latest_info using tavily
+# search the latest info using tavily
 def search_latest_info(query):
-  """This function helps to give
-  latest search using tavily
-  based on given user query related resarch or contents"""
-
-  client = TavilyClient(api_key = TAVILY_API_KEY)
+  """this function helps to give
+  latest search info using tavily
+  based on given user query related research
+  or contents"""
+  client = TavilyClient(TAVILY_API_KEY)
   response = client.search(query)
   return response
 
-#======== STEP 6 USER INPUT============
-st.header("Write Prompt to Generate ppt or Image or fetch Latest news")
+#==============step 6 user input ====================
+st.header("write the prompt to generate ppt or image or fetch latest news")
+user_input = st.text_area("write here: ")
 
-user_input = st.text_area("Write Here: ")
-
-
-# Tool 2 Generate image using free api
-
-def generate_image(img_prompt, slide_no = 1 ):
-  """This function helps user to generate
-  image using free api, with given img_prompt"""
-
+def generate_image(img_prompt,slide_no = 1):
+  """this function helps user generate
+  image using free api key with given
+  image_prompt"""
   url = f"https://image.pollinations.ai/{img_prompt}"
 
   import requests as r
   content = r.get(url).content
-  with open(f"ai_image_{slide_no}.jpeg", 'wb') as f:
+  with open(f"ai_image_{slide_no}.jpeg",'wb') as f:
     f.write(content)
-
   from PIL import Image
   img = Image.open(f"ai_image_{slide_no}.jpeg")
   return url
 
-def run_agent (leader_agent , query):
-  prompt = f"""Based on below given Query,
-  your task is to call specific tool, first to
-  promptify user prompt, than call image tool, or
-  latest search if required.give slide dynamic , ui ux,
-  with creative design , keep help of function to generate image
-  based on given topic,
-  with no of slides asked
-  and embed thatin same html ppt
-  and using file handling embed this in output html, use java script function
-  to generate image using async func and threading and give output in HTML
-  user query given below:
-   """
+def agent_prompt(query):
+  """this function helps promptify the given user
+  query, suppose user needs ppt based on given qury by user it gives detailed professional prompt
+  to return the output"""
 
-  prompt = prompt + query
-  #prompt = agent_prompt(query+ prompt)
+  prompt = f"""give high professional prompt for below given prompt :-
+  you are a professional ppt designer based on user given query, your task is to create
+  professional html output prompt with no markdown
+  user query :- {query}"""
 
-  response = leader_agent.invoke({'messages':[{'role':'user',
-                                              'content':prompt}]})
+  resonse = model.invoke(prompt)
+  final_prompt = resonse.content[-1]['text']
 
-  code = response['messages'][-1].content[-1]['text']
+  with open("PPT_PROMPT.txt", 'w') as f:
+    f.write(final_prompt)
+  return final_prompt
+
+def run_agent(leader_agent, query):
+  prompt = f"""Based on Below given Query, your task is to call specific tool,
+  first to promptify user prompt, than call image tool, or latest search if
+  required.give slide dynamic, ui ux, with creative design, keep help of
+  function to generate image based on given topic, Generate image using
+   with no of slide asked
+  and imbed that in same html ppt and using file handling embed this in
+  output html, use java script function to generate image using async
+  func and threading and give output in HTML user query given below:"""
+  prompt += query
+  prompt = agent_prompt(prompt)
+  response = leader_agent.invoke({'messages': [{'role': 'user', 'content': prompt}]})
+  code = response['messages'] [-1].content[-1]['text']
   return code
 
-
-
-#============== step 7 :- Agent Call===========
-# Learder_agent creation
+#==================step 7 ========================
 if all (ALL_API):
   leader_agent = create_agent(
       model = model,
@@ -114,49 +112,51 @@ if all (ALL_API):
 else:
   st.error("must pass api key ")
 
-# =========== STEP 8 NAVBAR STREAMLIT==========
+
+#======================step no 8========================
 tab1,tab2,tab3 = st.tabs(["Generate Image",
-                          "Fetch Latest News",
+                          "Fetch latest news",
                           "Generate PPT"])
+
 if (user_input) and (leader_agent):
-  # TAB 1 CODE
+  #tab 1 code 
   with tab1:
-    if st.button("Generate Image", key = "Gen-Image"):
-      with st.spinner("Running Agent"):
-        try:
-          img= generate_image(user_input)
-          st.image(img)
-        except:
-              url = f"https://image.pollinations.ai/{img_prompt}"
-              time.sleep(4)
-              st.image(url)
-  # TAB 2 CODE
+   if st.button("Generate image",key = "Gen-Image"):
+     with st.spinner("Running agent"):
+       try:
+         img = generate_image(user_input)
+         st.image(img)
+       except:
+          url = f"https://image.pollinations.ai/{user_input}"
+          time.sleep(4)
+          st.image(url)
   with tab2:
-    if st.button("Generate Image", key = "Gen-Image"):
+    if st.button("Fetch News", key = "Fetch-News"):
       with st.spinner("Running Agent"):
         try:
-          prompt = "Give Multiple News in HTML card Format for topic" + user_input
-          response = leader_agent.invoke({'messages':[{'role':'user',
-                                              'content':prompt}]})
-          code = response['messages'][-1].content[-1]['text']
-          st.html(code, width = "stretch",
-                  unsafe_allow_javascript=True)
+          prompt = "Give Multiple news in HTML card Format for topic" + user_input
+          response = leader_agent.invoke({'messages':[{'role': 'user',
+                                                      'content': prompt}]})
+          code = response['messages'] [-1].content[-1]['text']
+          st.html(code, width="stretch",
+          unsafe_allow_javascript=True)
         except Exception as err:
           st.error(err)
   with tab3:
     if st.button("Generate PPT", key = "Gen-PPT"):
       with st.spinner("Running Agent"):
         try:
-          code = run_agent(leader_agent,user_input)
-          st.html(code, width = "stretch",
-                  unsafe_allow_javascript=True)
-          with open("ppt.html",'w') as f: 
+          code = run_agent(leader_agent, user_input)
+          st.html(code, width="stretch",
+          unsafe_allow_javascript=True)
+          #file savce
+          with open("ppt.html",'w') as f:
             f.write(code)
           st.download_button(label = "DOWNLOAD PPT",
-                             data = code,
-                             file_name = 'ppt.html',
-                             mime = 'text/html')
+                          data = code,
+                          file_name = 'ppt.html',
+                          mime = 'text/html')
         except Exception as err:
           st.error(err)
 else:
-  st.error("SOMETHING WENT WRONG!!")
+  st.error("something went wrong")
